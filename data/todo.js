b2db3eb08548a99a649f93ab802bc92ce83db09f
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-08 23:12:07 -0500

    added todo data

---
+++ data/todo.js
@@ -0,0 +1,20 @@
+const todo = [
+    {
+        id: 1,
+        task: "Get Groceries",
+        completed: false,
+    },
+    {
+        id: 2,
+        task: "Study Express",
+        completed: true,
+    },
+    {
+        id: 3,
+        task: "Complete HW #3",
+        completed: false,
+    }
+]
+
+
+module.exports = todo;
--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+const todo = require('./data/todo')
 
+
+app.get('/', (req, res)=>{
+    res.send('Todo List Home Page')
+})
 
 app.listen(port, ()=>{
     console.log(`server listening on: ${port}`)
