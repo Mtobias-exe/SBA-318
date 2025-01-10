4c7503077f3b5e1e672ab2293ab8c8664c0a6559
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-09 21:51:57 -0500

    added put and fixed post method

--- index.js
+++ index.js
@@ -24,7 +24,30 @@ app.get('/api/users', (req,res)=>{
     res.json(users)
 })
 
+app.post('/api/users', (req, res)=>{
+    if(req.body.name && req.body.username && req.body.email){
+        if(users.find((u)=> u.username == req.body.username)){
+            res.json({error: 'Username Already Taken'})
+            return
+        }
+
+        const user = {
+            id: users[users.length -1].id + 1,
+            name: req.body.name,
+            username: req.body.username,
+            email: req.body.email
+        };
+    
+        users.push(user)
+        res.json(users[users.length -1])
+    } else res.json({error: 'Insufficient Data'})
+});
+
+
 //get all todos
+
+
+
 app.get('/api/todo', (req, res)=>{
     res.json(todos);
 } )
@@ -35,14 +58,31 @@ app.get('/api/todo/:id', (req,res,next)=>{
     
 })
 
+
+
 app.post('/api/todo', (req, res)=>{
-    let body = req.params.body
-    console.log(body);
-    todos.push()
-    res.json([]);
+
+    let tasks = [];
+    const newId = todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+    const newTodo = {
+        id: newId,
+        task: req.body.task,
+        completed: false
+    }
+
+    todos.push(newTodo)
+    res.json(todos);
 })
 
 app.put('/api/todo/:id', (req, res)=>{
+    let todo = todos.find(todo => todo.id == req.params.id)
+    if (todo){
+        todo.task = req.body.task
+        todo.completed = req.body.completed
+        res.json([])
+    } else{
+        res.send('Given id does not exist')
+    }
     res.json([]);
 })
 
