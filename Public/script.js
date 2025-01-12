42702a48bd5d455aab0671fa62f528c4fefbdf8a
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-11 22:35:34 -0500

    submit to do

--- Public/script.js
+++ Public/script.js
@@ -3,14 +3,37 @@ const submitTodo = document.getElementById('submit-todo')
 
 const URL = "http://localhost:3000/api/todo"
 
-submitTodo.addEventListener('click', getInfo)
 
-async function getInfo(e) {
-    e.preventDefault();
-    const res = await fetch(URL, { method: 'GET' });
-    console.log(res)
-    const data = await res.json()
-    inputTodo.value = data.task
+// submitTodo.addEventListener('click', getInfo)
+
+// async function getInfo(e) {
+//     e.preventDefault();
+//     const res = await fetch(URL, { method: 'GET' });
+//     console.log(res)
+//     const data = await res.json()
+//     inputTodo.value = data.task
    
+// }
+
+submitTodo.addEventListener('click', submitTask)
+
+async function start(){
+    const res = await fetch(URL);
+    const data = await res.json();
+   
+}
+
+
+async function submitTask(event){
+    event.preventDefault();
+    const toDo = inputTodo.value 
+
+    if (toDo){
+        const res = await fetch(URL, {method: 'POST'});
+        const newtask = await res.json();
+        console.log(newtask);
+        start()
+    }
+    
 }
 
---
+++ README.md
@@ -0,0 +1,3 @@
+Express Server Application
+
+Hello, this is an attempt to making a todo list 
